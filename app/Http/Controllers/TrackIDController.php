d3a40ae92da43d8ad97adc4d67af59bd7d47a985
MatheusMiliorini <miliorinimatheus@gmail.com>
2018-09-13 14:58:52 -0300

    Alterado contador de acessos

--- app/Http/Controllers/TrackIDController.php
+++ app/Http/Controllers/TrackIDController.php
@@ -13,7 +13,20 @@
             $ativo = count(DB::select('SELECT * FROM trackIDs WHERE trackID=? AND ativo=?',[$id,1]));
             
             if ($ativo == 1) {
+                //Insere o acesso no banco
                 DB::insert("INSERT INTO acessos(trackID,data) VALUES(?,?)",[$id,date('Y-m-d H:i:s')]);
+
+                //Adiciona esse acesso como ultimo acesso do TrackID
+                DB::update("UPDATE trackIDs SET ultimo_acesso=? WHERE trackID=?",[date('Y-m-d H:i:s'),$id]);
+                
+                //Aumenta em 1 a quantidade de acessos
+                DB::update("UPDATE trackIDs SET contagem_acessos=contagem_acessos+1 WHERE trackID=?",[$id]);
+
+                //Verifica se é o primeiro acesso
+                $primeiro_acesso = DB::select("SELECT primeiro_acesso FROM trackIDs WHERE trackID=?",[$id]);
+                if ($primeiro_acesso->primeiro_acesso == "") {
+                    DB::update("UPDATE trackIDs SET primeiro_acesso=? WHERE trackID=?",[date('Y-m-d H:i:s'),$id]);
+                }
             }
 
             return view('trackID');
